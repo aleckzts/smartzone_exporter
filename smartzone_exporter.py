e27bf73ef8b0a51b3f976dc37918360b1a298441
s165433 <aleck.sousa@trt18.jus.br>
2025-02-12 09:44:58 -0300

    encode password

--- smartzone_exporter.py
+++ smartzone_exporter.py
@@ -2,6 +2,7 @@
 import requests
 import os
 import sys
+import urllib.parse
 
 # Allow for silencing insecure warnings from requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
@@ -294,7 +295,7 @@ class SmartZoneCollector():
                     if wlan_data['encryption']['method'] == 'WPA2':
                         details_metrics['passphrase'].add_metric([zone_name, str(wlan['name']), wlan['ssid'], wlan_data['encryption']['passphrase']], 1)
                         details_metrics['qrcode'].add_metric([zone_name, str(wlan['name']), wlan['ssid'],
-                            'https://api.qrserver.com/v1/create-qr-code/?size=350x350&data=WIFI:T:WPA;S:{};P:{} ;;'.format(wlan['ssid'], wlan_data['encryption']['passphrase'])
+                            'https://api.qrserver.com/v1/create-qr-code/?size=350x350&data=WIFI:T:WPA;S:{};P:{};;'.format(wlan['ssid'], urllib.parse.quote_plus(wlan_data['encryption']['passphrase']))
                             ], 1)
                     if wlan_data['schedule']['type'] == 'AlwaysOff':
                         details_metrics['schedule'].add_metric([zone_name, str(wlan['name']), wlan['ssid'], "AlwaysOff", "-", "-", "-", "-", "-", "-", "-" ], 0)
